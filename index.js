791b242ac128df73c4e8b376cd0dc25c020abb32
Austin Miller <amiller@my.ccsu.edu>
2018-08-16 04:09:01 -0400

    Reduced to minimum through refactoring
    index.js, which used to hold a large chunk of the application's code,
    has been made quite bare through the introduction of TwitchBot.js and
    further changes on DialogflowController.js. All code related to either
    the twitch functionality or the dialogflow agent functioanlity (besides
    initialization and relationship code) has been moved out of index.js.

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-//Diaglogflow Request code taken from https://github.com/dialogflow/dialogflow-nodejs-client-v2#quickstart
-
 'use strict';
 
 //modules
 const fs = require('fs');   //to read JSON files
 const dfc = require('./dialogflow/DialogflowController.js'); //our simple interface to Dialogflow
-const tmi = require('tmi.js'); //for twitch integration
+const tb = require('./twitch/TwitchBot.js'); //represents the twitch bot
 
 //Load properties
 const propertyObject = JSON.parse(fs.readFileSync('properties.json'));
 
-//Dialogflow details
+//Dialogflow setup
 const dfConfig = propertyObject.dialogflowConfig;
 const dfController = new dfc(dfConfig); //controller provides access to the agent
 
-//Twitch bot details
+//Twitch bot setup
 const twitchBotConfig = propertyObject.twitchBotConfig;
-const twitchClient = new tmi.client(twitchBotConfig);
-twitchClient.connect();
-
-//Print function for dialogflow response
-function printResponse(response){
-    console.log("Query: " + response.queryText);
-    console.log("Response: " + response.fulfillmentText);
-}
-
-//Function to run when chat message is received
-function onMessage(channel, userstate, message, self){
-    console.log(`message '${message}' from ${userstate['username']}`);
-    console.log("sending to diaglogflow...");
-    dfController.queryAgent(message, printResponse);
-}
-twitchClient.on("message", onMessage);
-
-
+const twitchBot = new tb(twitchBotConfig, dfController);
+twitchBot.start();
