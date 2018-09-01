c4016763fac69444aa5a1a66e6f399d1c8bd72a7
Austin Miller <amiller@my.ccsu.edu>
2018-08-31 21:59:11 -0400

    Refactored DFController to use Class
    Previously, DialogFlowController was a type of object defined by its
    prototype. This has been changed to be defined as a class instead, as
    that seems more intuitive to the design than using prototypes. Other
    small refactorings were done.

--- dialogflow/DialogflowController.js
+++ dialogflow/DialogflowController.js
@@ -2,6 +2,59 @@
 'use strict';
 const df = require('dialogflow');
 
+class DialogflowController{
+
+    constructor(config){
+        this.languageCode = 'en-US'; //Default language code
+
+        //Instance vars
+        this.agentId = config.agentId;
+        this.sessionId = config.sessionId;
+
+        //Client set up instance vars
+        this.sessionClient = new df.SessionsClient(); //set up client
+        this.sessionPath = this.sessionClient.sessionPath(this.agentId, this.sessionId);//set up session path
+
+        //Bind methods
+        this.queryAgent = this.queryAgent.bind(this);
+    }
+
+    queryAgent(queryString, callback){
+        //Base case (query is empty, is whitespace or does not exist)
+        if(!queryString || !queryString.trim()) return;
+    
+        //Create and send query, perform callback on response json
+        this._sendQuery.call(this, this._createQuery.call(this, queryString), callback);
+    }
+
+    //Creates query request object given string
+    _createQuery(query){
+        return {
+            session: this.sessionPath,
+            queryInput: {
+                text: {
+                    text: query,
+                    languageCode: this.languageCode
+                }
+            }
+        };
+    }
+
+    //Sends query
+    _sendQuery(req, callback){
+        this.sessionClient
+            .detectIntent(req) //perform a query request
+            .then(responses => {
+                const result = responses[0].queryResult; //Get result
+
+                //TODO, returning fulfillment text only removes access to a lot of the returned response.
+                callback(result.fulfillmentText); //Run function on result
+            })
+            .catch(err => console.log('An error has occurred', err));
+    }
+}
+
+/*
 //DialogflowController will have major functionality relating to Dialogflow (such as making and sending requests)
 function DialogflowController(config){
     //Instance vars
@@ -49,7 +102,7 @@ DialogflowController.prototype.queryAgent = function(queryString, callback){
 
     //Create and send query, perform callback on response json
     sendQuery.call(this, createQuery.call(this, queryString), callback);
-}
+}*/
 
 //Export
 module.exports = DialogflowController;
