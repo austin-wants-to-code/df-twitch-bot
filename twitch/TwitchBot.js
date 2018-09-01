636733a6123c9513fe7410aa77d777e417bd5042
Austin Miller <amiller@my.ccsu.edu>
2018-08-31 22:17:10 -0400

    TwitchBot to be Class
    Like DFController, TwitchBot was defined through its prototype. This has
    been changed to be using a class, which makes the design clearer. Small
    refactors were made. Works as the old version did.

--- twitch/TwitchBot.js
+++ twitch/TwitchBot.js
@@ -1,53 +1,53 @@
 'use strict';
 const tmi = require('tmi.js');
 
-function TwitchBot(botConfig, dfController){
-    this.client = new tmi.client(botConfig); //The client, i.e., the bot itself
-    this.dfController = dfController;
-}
 
-TwitchBot.prototype.start = function(){
-    this.client.connect(); //Connect to the channels
+class TwitchBot{
 
-    /* private functions to handle each message */
-
-    //Posts given message to chat
-    function postResponseToChat(channel, responseText){
-        this.client.say(channel, responseText);
+    constructor(botConfig, dfController){
+        this.client = new tmi.client(botConfig); //The client, i.e., the bot itself
+        this.dfController = dfController;
     }
 
-    //Sends given message to dialogflow agent and runs callback on response text
-    function sendMessageToDialogflow(message, callback){
-        this.dfController.queryAgent(message, callback)
+    start(){
+        this.client.connect(); //Connect to the channels
+        this.client.on("message", this._onMessage.bind(this));
+    }
+
+    //On worthy messages, print the details locally, send to dialogflow agent and send agent's response to chat
+    _onMessage(channel, userstate, message, self){
+        //ignore unworthy messages
+        if(!this._isMessageWorthy(message) || self) return;
+    
+        //Print message details
+        let senderUsername = this._getUser(userstate);
+        console.log(`message '${message}' from ${senderUsername} on channel ${channel}`);
+        console.log("sending to diaglogflow...");
+    
+        //Send message to dialogflow, print to chat
+        this._sendMessageToDialogflow(message, this._postResponseToChat.bind(this, channel));
     }
 
     //Returns true only if message is worthy enough for agent
-    function isMessageWorthy(message){
+    _isMessageWorthy(message){
         return (message     //message exists
             && message.trim().length > 0 //message isn't whitespace
             && message.length <= 25); //message isn't more than 25 char (problem with emotes???)
     }
 
     //Gets username given userstate
-    function getUser(userstate){
+    _getUser(userstate){
         return userstate['username'];
     }
 
-    //On worthy messages, print the details locally, send to dialogflow agent and send agent's response to chat
-    function onMessage(channel, userstate, message, self){
-        //ignore unworthy messages
-        if(!isMessageWorthy(message) || self) return;
-
-        //Print message details
-        let senderUsername = getUser(userstate);
-        console.log(`message '${message}' from ${senderUsername} on channel ${channel}`);
-        console.log("sending to diaglogflow...");
-
-        //Send message to dialogflow, print to chat
-        sendMessageToDialogflow.call(this, message, postResponseToChat.bind(this, channel));
-
+    _postResponseToChat(channel, responseText){
+        this.client.say(channel, responseText);
     }
-    this.client.on("message", onMessage.bind(this));
+
+    //Sends given message to dialogflow agent and runs callback on response text
+    _sendMessageToDialogflow(message, callback){
+        this.dfController.queryAgent(message, callback);
+    }
 }
 
 //Export
